24f6fb6c8c7dfd3e0f406153d24e299de5686f1f
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2023-05-16 15:30:15 +0800

    SSD-Idb2 first update

---
+++ app/Http/Controllers/idb2dataController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class idb2dataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $qry = DB::connection(env('DB2_CONNECTION'))->select('SELECT STSHRT, STADD3 , STCITY , STZIP , STPHON FROM MM770QAL.TBLSTR');
+
+        foreach ($qry as $item) {
+            // * $data is a Collection of ApiResources merged together per Page.
+            $item->stshrt = rtrim($item->stshrt);
+            $item->stadd3 = rtrim($item->stadd3);
+            $item->stcity = rtrim($item->stcity);
+            $item->stzip = rtrim($item->stzip);
+            $item->stphon = rtrim($item->stphon);
+        }
+
+        //  $qry = DB::connection(env('DB2_CONNECTION'))->select('SELECT POSTAT,PONOT1,POVNUM, PONUMB, POEDAT FROM MM770QAL.POMHDR');
+
+        return response()->json($qry);
+    }
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
